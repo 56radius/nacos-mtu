2e5a2bac14220be494f8b87ddbf713096abbd40e
Merit Mohammed <cussmeritmohammed@gmail.com>
2024-04-10 22:18:55 +0100

    Adding media query for responsiveness and sectioning images

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -138,45 +138,33 @@ function HomeScreen() {
 
 
      {/* First About */}
-      <section id="about" className="about">
-        <div className="container">
-          <div className="section-title">
-           
+     <section id="about" className="about">
+  <div className="container">
+    <div className="section-title"></div>
+    <div className="row content">
+      <div className="col-lg-6">
+        <div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img src={question} alt="question" style={{ marginRight: "20px" }} />
+            <div>
+              <h2 style={{ color: "black" }}> About NACOS </h2>
+              <h3 style={{ fontFamily: "Cabinet Grotesk" }}> Brief History of NACOS NATIONAL </h3>
+            </div>
           </div>
-          <div className="row content">
-          <div className="col-lg-6">
-              <div >
-              <div>
-                <img src={question} alt="question" />
-                <h2 style={{color: "black"}}> About NACOS </h2>
-                <p style={{fontFamily: "Cabinet Grotesk"}}> Brief History of NACOS NATIONAL </p>
-              </div>
-              <p style={{fontFamily: "Cabinet Grotesk", fontSize: "140%"}}>
-              The Nigerian Association of Computing Students (NACOS) is a student professional body with presence in almost all tertiary institutions in Nigeria (both private and government owned). NACOS was founded by groups of students in July 1993 with the backing of Nigerian Computer Society (NCS) as its parent body. It provides avenues for students (in any IT related field) to highlight and champion issues of interest in a coordinated and organized manner.
-                
-              </p>
-           
-              </div>
-            </div>
-          <div className="col-lg-6 pt-4 pt-lg-0">
-          <div className="col-lg-6 order-1 order-lg-2 hero-img">
-              
-              <img
-                src={aboutlogo}
-                style={{width: 1350}}
-                className="img-fluid"
-                alt="aboutlogo"
-              />
-         
-            </div>
-
-             
-            </div>
-     
-            
-          </div>
+          <p style={{ fontFamily: "Cabinet Grotesk", fontSize: "140%" }}>
+            The Nigerian Association of Computing Students (NACOS) is a student professional body with presence in almost all tertiary institutions in Nigeria (both private and government owned). NACOS was founded by groups of students in July 1993 with the backing of Nigerian Computer Society (NCS) as its parent body. It provides avenues for students (in any IT related field) to highlight and champion issues of interest in a coordinated and organized manner.
+          </p>
         </div>
-      </section>
+      </div>
+      <div className="col-lg-6 pt-4 pt-lg-0">
+        <div className="col-lg-6 order-1 order-lg-2 hero-img">
+          <img src={aboutlogo} style={{ width: 1350 }} className="img-fluid" alt="aboutlogo" />
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 
       {/* Secomd About section */}
       <section id="about" className="about">
@@ -223,82 +211,14 @@ function HomeScreen() {
       <section id="services" className="services">
         <div className="container" data-aos="fade-up">
           <div className="section-title">
-            <h2>Services</h2>
+            <h2> Executives </h2>
           </div>
 
-          <div className="row">
-            <div
-              className="col-lg-4 col-md-6 d-flex align-items-stretch"
-              data-aos="zoom-in"
-              data-aos-delay="100"
-            >
-              <div className="icon-box">
-                <div className="icon">
-                  <i className="bx bxl-dribbble"></i>
-                </div>
-                <h4>
-                  <a href=""> Mindfulness Meditation </a>
-                </h4>
-                <p>
-                  {" "}
-                  Our Mindfulness meditation program utilizes AI technology to
-                  deliver personalized guided meditations and regular meditation
-                  practice that fits your lifestyle and supports your mental
-                  health.{" "}
-                </p>
-              </div>
-            </div>
-
-            <div
-              className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0"
-              data-aos="zoom-in"
-              data-aos-delay="200"
-            >
-              <div className="icon-box">
-                <div className="icon">
-                  <i className="bx bx-file"></i>
-                </div>
-                <h4>
-                  <a href=""> Mindfulness-Based Cognitive Therapy </a>
-                </h4>
-                <p>
-                  {" "}
-                  Our mindfulness-based cognitive therapy program helps you
-                  manage negative thoughts, emotions and behaviours.{" "}
-                </p>
-              </div>
-            </div>
-
-            <div
-              className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0"
-              data-aos="zoom-in"
-              data-aos-delay="300"
-            >
-              <div className="icon-box">
-                <div className="icon">
-                  <i className="bx bx-tachometer"></i>
-                </div>
-                <h4>
-                  <a href=""> Mindfulness-Based Stress Reduction </a>
-                </h4>
-                <p>
-                  {" "}
-                  Offers a holistic approach to stress management that
-                  incorporates mindfulness practices, yoga and body awareness
-                  techniques.{" "}
-                </p>
-              </div>
-            </div>
-          </div>
+      
         </div>
       </section>
 
-      {/* parallax effect */}
-      <section style={{ position: "relative", height: 700 }}>
-        <div class="p-one parallax-inner">
-          <h2 style={{ fontWeight: "bold" }}>AVINX HEALTH</h2>
-        </div>
-      </section>
+   
 
       {/* Contact */}
       <section id="contact" className="about">
